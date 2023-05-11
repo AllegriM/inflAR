6f9080ee85efa703ad58f1b077c04662033a0efc
AllegriM <elmonky99@gmail.com>
2023-05-10 23:38:40 -0300

    refactor: :closed_lock_with_key: add API url as a env variable

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -2,10 +2,11 @@
 export const fetchApi = async (): Promise<IPCProps[]> => {
   const response = await fetch(
     // "/src/data/ipc.json",
-    "https://apis.datos.gob.ar/series/api/series/?ids=148.3_INIVELNAL_DICI_M_26&limit=5000",
+    import.meta.env.VITE_API_URL as string,
   );
   const {data} = await response.json();
-  const newData = data.map((item: [string, number]) => {
+
+  const newData = data?.map((item: [string, number]) => {
     return {
       fecha: item[0],
       IPC: item[1],
